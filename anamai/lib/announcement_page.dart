0e53b20a4d2e7d3104bed3dc1c2dc4c19d79391d
ittiwatwhangdee <fahut.ittiwat@gmail.com>
2022-02-01 19:29:37 +0700

    add itembuilder card

--- anamai/lib/announcement_page.dart
+++ anamai/lib/announcement_page.dart
@@ -41,114 +41,90 @@ class MyStatelessWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: ListView(
-          children: [
-            Card(
-              clipBehavior: Clip.antiAlias,
-              child: Column(
-                children: [
-                  ListTile(
-                    leading: Image.asset('assets/images/LogoAnamai.png'),
-                    title: const Text('มาตรการปลอดภัยสำหรับองค์กร'),
-                    subtitle: Text(
-                      'วันอังคารที่ 2 กุมภาพันธ์ พ.ศ.2565',
-                      style: TextStyle(color: Colors.black.withOpacity(0.6)),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Text(
-                      'ข้อเสนอการยกระดับมาตรการ กรณีเปิดสถานประกอบการในพื้นที่ควบคุมสูงสุดและเข้มงวด มาตรการปลอดภัยสำหรับองค์กร (Covid Free Setting)',
-                      style: TextStyle(color: Colors.black.withOpacity(0.6)),
-                    ),
-                  ),
-                  ButtonBar(
-                    alignment: MainAxisAlignment.start,
+          padding: const EdgeInsets.all(8.0),
+          child: ListView.builder(
+            itemCount: 3,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Card(
+                  clipBehavior: Clip.antiAlias,
+                  child: Column(
                     children: [
-                      FlatButton(
-                        onPressed: () {
-                          // Perform some action
-                        },
-                        child: const Text('อ่านรายละเอียด'),
+                      Image.asset('assets/images/Announ1.png'),
+                      ListTile(
+                        // leading: Image.asset('assets/images/LogoAnamai.png'),
+                        title: const Text('บะหมี่กึ่งสำเร็จรูปโซเดียมสูง'),
+                        subtitle: Text(
+                          'วันอังคารที่ 2 กุมภาพันธ์ พ.ศ.2565',
+                          style:
+                              TextStyle(color: Colors.black.withOpacity(0.6)),
+                        ),
                       ),
+                      // Padding(
+                      //   padding: const EdgeInsets.all(16.0),
+                      //   child: Text(
+                      //     'กรมอนามัย เตือน บะหมี่กึ่งสำเร็จรูปโซเดียมสูง กินบ่อย เสี่ยง ‘ความดันสูง – หัวใจวาย’',
+                      //     style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                      //   ),
+                      // ),
+                      ButtonBar(
+                        alignment: MainAxisAlignment.start,
+                        children: [
+                          FlatButton(
+                            onPressed: () {
+                              // Perform some action
+                            },
+                            child: const Text('อ่านเพิ่มเติม'),
+                          ),
+                        ],
+                      ),
+                      // Image.asset('assets/images/Announ1.png'),
                     ],
                   ),
-                  // Image.asset('assets/images/Announ1.png'),
-                ],
-              ),
-            ),
-            Card(
-              clipBehavior: Clip.antiAlias,
-              child: Column(
-                children: [
-                  ListTile(
-                    leading: Image.asset('assets/images/LogoAnamai.png'),
-                    title: const Text('บะหมี่กึ่งสำเร็จรูปโซเดียมสูง'),
-                    subtitle: Text(
-                      'วันอังคารที่ 2 กุมภาพันธ์ พ.ศ.2565',
-                      style: TextStyle(color: Colors.black.withOpacity(0.6)),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Text(
-                      'กรมอนามัย เตือน บะหมี่กึ่งสำเร็จรูปโซเดียมสูง กินบ่อย เสี่ยง ‘ความดันสูง – หัวใจวาย’',
-                      style: TextStyle(color: Colors.black.withOpacity(0.6)),
-                    ),
-                  ),
-                  ButtonBar(
-                    alignment: MainAxisAlignment.start,
-                    children: [
-                      FlatButton(
-                        onPressed: () {
-                          // Perform some action
-                        },
-                        child: const Text('อ่านรายละเอียด'),
-                      ),
-                    ],
-                  ),
-                  // Image.asset('assets/images/LogoAnamai.png'),
-                ],
-              ),
-            ),
-            Card(
-              clipBehavior: Clip.antiAlias,
-              child: Column(
-                children: [
-                  ListTile(
-                    leading: Image.asset('assets/images/LogoAnamai.png'),
-                    title: const Text('บะหมี่กึ่งสำเร็จรูปโซเดียมสูง'),
-                    subtitle: Text(
-                      'วันอังคารที่ 2 กุมภาพันธ์ พ.ศ.2565',
-                      style: TextStyle(color: Colors.black.withOpacity(0.6)),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Text(
-                      'กรมอนามัย เตือน บะหมี่กึ่งสำเร็จรูปโซเดียมสูง กินบ่อย เสี่ยง ‘ความดันสูง – หัวใจวาย’',
-                      style: TextStyle(color: Colors.black.withOpacity(0.6)),
-                    ),
-                  ),
-                  ButtonBar(
-                    alignment: MainAxisAlignment.start,
-                    children: [
-                      FlatButton(
-                        onPressed: () {
-                          // Perform some action
-                        },
-                        child: const Text('อ่านรายละเอียด'),
-                      ),
-                    ],
-                  ),
-                  // Image.asset('assets/images/LogoAnamai.png'),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
+                ),
+              );
+            },
+          )
+          // ListView(
+          //   children: [
+          //     Card(
+          //       clipBehavior: Clip.antiAlias,
+          //       child: Column(
+          //         children: [
+          //           ListTile(
+          //             leading: Image.asset('assets/images/LogoAnamai.png'),
+          //             title: const Text('มาตรการปลอดภัยสำหรับองค์กร'),
+          //             subtitle: Text(
+          //               'วันอังคารที่ 2 กุมภาพันธ์ พ.ศ.2565',
+          //               style: TextStyle(color: Colors.black.withOpacity(0.6)),
+          //             ),
+          //           ),
+          //           Padding(
+          //             padding: const EdgeInsets.all(16.0),
+          //             child: Text(
+          //               'ข้อเสนอการยกระดับมาตรการ กรณีเปิดสถานประกอบการในพื้นที่ควบคุมสูงสุดและเข้มงวด มาตรการปลอดภัยสำหรับองค์กร (Covid Free Setting)',
+          //               style: TextStyle(color: Colors.black.withOpacity(0.6)),
+          //             ),
+          //           ),
+          //           ButtonBar(
+          //             alignment: MainAxisAlignment.start,
+          //             children: [
+          //               FlatButton(
+          //                 onPressed: () {
+          //                   // Perform some action
+          //                 },
+          //                 child: const Text('อ่านรายละเอียด'),
+          //               ),
+          //             ],
+          //           ),
+          //           // Image.asset('assets/images/Announ1.png'),
+          //         ],
+          //       ),
+          //     ),
+          //   ],
+          // ),
+          ),
     );
   }
 }
