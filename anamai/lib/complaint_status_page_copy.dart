b78a1aa5c54fa2651094c2170e8fd1108bb64a50
ittiwatwhangdee <fahut.ittiwat@gmail.com>
2022-02-04 16:18:02 +0700

    Update complaint_status_page_copy.dart

--- anamai/lib/complaint_status_page_copy.dart
+++ anamai/lib/complaint_status_page_copy.dart
@@ -44,9 +44,9 @@ class MyStatelessWidget extends StatefulWidget {
 }
 
 class _MyStatelessWidgetState extends State<MyStatelessWidget> {
-  final List<Map> _products = List.generate(5, (i) {
+  final List<Map> _products = List.generate(20, (i) {
     return {
-      "id": i + 1,
+      "id": i,
       "title": "หัวข้อ $i",
       "name": "เรื่องร้องเรียน $i",
       "date": "2 ธันวาคม 2565",
@@ -67,106 +67,100 @@ class _MyStatelessWidgetState extends State<MyStatelessWidget> {
       scrollDirection: Axis.vertical,
       child: SingleChildScrollView(
         scrollDirection: Axis.horizontal,
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: DataTable(
-            sortColumnIndex: _currentSortColumn,
-            sortAscending: _isAscending,
-            headingRowColor: MaterialStateProperty.all(Colors.grey[200]),
-            columns: [
-              const DataColumn(
-                  label: Text('ลำดับ',
-                      style: TextStyle(fontWeight: FontWeight.bold))),
-              const DataColumn(
-                  label: Text('หัวข้อการร้องเรียน',
-                      style: TextStyle(fontWeight: FontWeight.bold))),
-              const DataColumn(
-                  label: Text('เรื่องร้องเรียน',
-                      style: TextStyle(fontWeight: FontWeight.bold))),
-              const DataColumn(
-                  label: Text('วันที่ร้องเรียน',
-                      style: TextStyle(fontWeight: FontWeight.bold))),
-              const DataColumn(
-                  label: Text('สถานะการร้องเรียน',
-                      style: TextStyle(fontWeight: FontWeight.bold))),
-              const DataColumn(
-                  label: Text('แนบเอกสาร',
-                      style: TextStyle(fontWeight: FontWeight.bold))),
-            ],
-            rows: _products.map((item) {
-              return DataRow(cells: [
-                DataCell(Text(item['id'].toString())),
-                DataCell(Text(item['title'])),
-                DataCell(Text(item['name'].toString())),
-                DataCell(Text(item['date'].toString())),
-                DataCell(Text(
-                  item['status'].toString(),
-                  style: TextStyle(
-                      color: Colors.deepPurple[300],
-                      fontWeight: FontWeight.bold),
-                )),
-                DataCell(IconButton(
-                  icon: Icon(Icons.attach_file),
-                  onPressed: () {
-                    showModalBottomSheet(
-                        enableDrag: true,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.vertical(
-                            top: Radius.circular(20),
-                          ),
+        child: DataTable(
+          sortColumnIndex: _currentSortColumn,
+          sortAscending: _isAscending,
+          headingRowColor: MaterialStateProperty.all(Colors.grey[200]),
+          columns: [
+            const DataColumn(
+                label: Text('ลำดับ',
+                    style: TextStyle(fontWeight: FontWeight.bold))),
+            const DataColumn(
+                label: Text('หัวข้อการร้องเรียน',
+                    style: TextStyle(fontWeight: FontWeight.bold))),
+            const DataColumn(
+                label: Text('เรื่องร้องเรียน',
+                    style: TextStyle(fontWeight: FontWeight.bold))),
+            const DataColumn(
+                label: Text('วันที่ร้องเรียน',
+                    style: TextStyle(fontWeight: FontWeight.bold))),
+            const DataColumn(
+                label: Text('สถานะการร้องเรียน',
+                    style: TextStyle(fontWeight: FontWeight.bold))),
+            const DataColumn(
+                label: Text('แนบเอกสาร',
+                    style: TextStyle(fontWeight: FontWeight.bold))),
+          ],
+          rows: _products.map((item) {
+            return DataRow(cells: [
+              DataCell(Text(item['id'].toString())),
+              DataCell(Text(item['title'])),
+              DataCell(Text(item['name'].toString())),
+              DataCell(Text(item['date'].toString())),
+              DataCell(Text(
+                item['status'].toString(),
+                style: TextStyle(
+                    color: Colors.deepPurple[300], fontWeight: FontWeight.bold),
+              )),
+              DataCell(IconButton(
+                icon: Icon(Icons.attach_file),
+                onPressed: () {
+                  showModalBottomSheet(
+                      enableDrag: true,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.vertical(
+                          top: Radius.circular(20),
                         ),
-                        backgroundColor: Colors.transparent,
-                        barrierColor: Colors.black38,
-                        context: context,
-                        builder: (context) => Container(
-                              height: 350,
-                              child: Container(
-                                  decoration: BoxDecoration(
-                                      color: Colors.white,
-                                      borderRadius: BorderRadius.only(
-                                          topLeft: const Radius.circular(18.0),
-                                          topRight:
-                                              const Radius.circular(18.0))),
-                                  child: Center(
-                                    child: Column(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      children: [
-                                        Text(
-                                          'แนบไฟล์ของคุณ',
-                                          style: TextStyle(fontSize: 16),
-                                        ),
-                                        Text(
-                                          '* ขนาดไฟล์ไม่เกิน 2GB',
-                                          style: TextStyle(
-                                              fontSize: 14, color: Colors.grey),
-                                        ),
-                                        SizedBox(
-                                          height: 7,
-                                        ),
-                                        RaisedButton(
-                                          onPressed: () {
-                                            print('Select Upload');
-                                          },
-                                          color: Colors.blueAccent,
-                                          shape: RoundedRectangleBorder(
-                                              borderRadius:
-                                                  BorderRadius.circular(10.0)),
-                                          child: Text('อัพโหลด',
-                                              style: TextStyle(
-                                                  fontSize: 15,
-                                                  color: Colors.white,
-                                                  fontWeight: FontWeight.bold)),
-                                        )
-                                      ],
-                                    ),
-                                  )),
-                            ));
-                  },
-                )),
-              ]);
-            }).toList(),
-          ),
+                      ),
+                      backgroundColor: Colors.transparent,
+                      barrierColor: Colors.black38,
+                      context: context,
+                      builder: (context) => Container(
+                            height: 350,
+                            child: Container(
+                                decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.only(
+                                        topLeft: const Radius.circular(18.0),
+                                        topRight: const Radius.circular(18.0))),
+                                child: Center(
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Text(
+                                        'แนบไฟล์ของคุณ',
+                                        style: TextStyle(fontSize: 16),
+                                      ),
+                                      Text(
+                                        '* ขนาดไฟล์ไม่เกิน 2GB',
+                                        style: TextStyle(
+                                            fontSize: 14, color: Colors.grey),
+                                      ),
+                                      SizedBox(
+                                        height: 7,
+                                      ),
+                                      RaisedButton(
+                                        onPressed: () {
+                                          print('Select Upload');
+                                        },
+                                        color: Colors.blueAccent,
+                                        shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(10.0)),
+                                        child: Text('อัพโหลด',
+                                            style: TextStyle(
+                                                fontSize: 15,
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.bold)),
+                                      )
+                                    ],
+                                  ),
+                                )),
+                          ));
+                },
+              )),
+            ]);
+          }).toList(),
         ),
       ),
     )));
