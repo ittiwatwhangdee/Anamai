4a79edeaf971f70582b3e4a7706ab6d4cc750a45
ittiwatwhangdee <fahut.ittiwat@gmail.com>
2022-02-08 09:34:18 +0700

    edit notification page to item count

--- anamai/lib/pages/notifications/noti_news_page.dart
+++ anamai/lib/pages/notifications/noti_news_page.dart
@@ -9,35 +9,35 @@ class NotificationNewsPage extends StatefulWidget {
 
 class _NotificationNewsPageState extends State<NotificationNewsPage> {
   final icons = [Icons.ac_unit, Icons.access_alarm, Icons.access_time];
+  final String title =
+      'บัตรพนักงานเจ้าหน้าที่ตาม พ.ร.บควบคุมการส่งเสริมการตลาดอาหารสำหรับทารกและเด็ก';
+  final String date = '2 กุมภาพันธ์ พ.ศ.2565';
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: const <Widget>[
-        Card(
-          child: ListTile(
-            leading: CircleAvatar(
-              backgroundColor: Colors.white,
-              backgroundImage: AssetImage(
-                  "assets/images/LogoAnamai.png"), // no matter how big it is, it won't overflow
-            ),
-            title: Text('กรมอนามัย'),
-            subtitle: Text('แจ้งเตือนโรคซอมบี้ - วันที่ 2 กุมภาพันธ์ 2565'),
-            isThreeLine: true,
-          ),
-        ),
-        Card(
-          child: ListTile(
-            leading: CircleAvatar(
-              backgroundColor: Colors.white,
-              backgroundImage: AssetImage(
-                  "assets/images/LogoAnamai.png"), // no matter how big it is, it won't overflow
-            ),
-            title: Text('กรมอนามัย'),
-            subtitle: Text('แจ้งเตือนโรคซอมบี้ - วันที่ 2 กุมภาพันธ์ 2565'),
-            isThreeLine: true,
-          ),
-        ),
-      ],
+    return Scaffold(
+      body: Padding(
+          padding: const EdgeInsets.all(0),
+          child: ListView.builder(
+            itemCount: 2,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.all(0),
+                child: Card(
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundColor: Colors.white,
+                      backgroundImage:
+                          AssetImage("assets/images/LogoAnamai.png"),
+                    ),
+                    title: Text('กรมอนามัย'),
+                    subtitle:
+                        Text('แจ้งเตือนโรคซอมบี้ - วันที่ 2 กุมภาพันธ์ 2565'),
+                    isThreeLine: true,
+                  ),
+                ),
+              );
+            },
+          )),
     );
   }
 }
