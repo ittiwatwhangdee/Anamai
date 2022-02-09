9299a46d8fc88a591c9596f43f5c2d8496081290
ittiwatwhangdee <fahut.ittiwat@gmail.com>
2022-02-09 11:36:17 +0700

    change my_card_page & news_page

--- anamai/lib/pages/notifications/noti_page.dart
+++ anamai/lib/pages/notifications/noti_page.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import '../../home_page.dart';
-import 'noti_mycard_page.dart';
-import 'noti_news_page.dart';
 
 void main() => runApp(const NotificationPage());
 
@@ -68,5 +66,62 @@ class _NotificationPageState extends State<NotificationPage>
         ],
       ),
     );
+  }
+}
+
+class NotificationNewsPage extends StatefulWidget {
+  const NotificationNewsPage({Key? key}) : super(key: key);
+
+  @override
+  _NotificationNewsPageState createState() => _NotificationNewsPageState();
+}
+
+class _NotificationNewsPageState extends State<NotificationNewsPage> {
+  final icons = [Icons.ac_unit, Icons.access_alarm, Icons.access_time];
+  final String title =
+      'บัตรพนักงานเจ้าหน้าที่ตาม พ.ร.บควบคุมการส่งเสริมการตลาดอาหารสำหรับทารกและเด็ก';
+  final String date = '2 กุมภาพันธ์ พ.ศ.2565';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+          padding: const EdgeInsets.all(0),
+          child: ListView.builder(
+            itemCount: 2,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.all(0),
+                child: Card(
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundColor: Colors.white,
+                      backgroundImage:
+                          AssetImage("assets/images/LogoAnamai.png"),
+                    ),
+                    title: Text('กรมอนามัย'),
+                    subtitle:
+                        Text('แจ้งเตือนโรคซอมบี้ - วันที่ 2 กุมภาพันธ์ 2565'),
+                    isThreeLine: true,
+                  ),
+                ),
+              );
+            },
+          )),
+    );
+  }
+}
+
+class NotificationMyCardPage extends StatefulWidget {
+  const NotificationMyCardPage({Key? key}) : super(key: key);
+
+  @override
+  _NotificationMyCardPageState createState() => _NotificationMyCardPageState();
+}
+
+class _NotificationMyCardPageState extends State<NotificationMyCardPage> {
+  bool pressAttention = false;
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView();
   }
 }
