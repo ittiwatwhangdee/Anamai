e50591ad2ea29d8624b1089896853b8d056ba919
ittiwatwhangdee <fahut.ittiwat@gmail.com>
2022-02-23 09:30:04 +0700

    Update main.dart

--- anamai/lib/main.dart
+++ anamai/lib/main.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'home_page.dart';
-import 'splash_page.dart';
 import 'login_page.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    theme: ThemeData(fontFamily: 'SFProTH_regular'), // home: SplashScreen(),
+    theme: ThemeData(fontFamily: 'SFProTH_regular'),
     home: LoginPage(),
   ));
 }
